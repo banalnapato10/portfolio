230c8cd8df23b459013c092f3aa8d83c1d9e671a
Jamil Riotoc <jamil.riotoc@gmail.com>
2021-03-08 19:38:18 +0800

    updated content of component jumbo 2

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -8,7 +8,7 @@ export default function Jumbo() {
         <Jumbotron>
             <h1>Batch 101 - Hello World!</h1>
             <p>Ex aute dolor ut occaecat magna sed ea occaecat ullamco cillum veniam dolor sit labore in laborum sed elit aute laborum fugiat voluptate sit sunt non labore occaecat sit nostrud occaecat dolore excepteur sed.</p>
-            <Button variant="primary">Learn more</Button>
+            <Button variant="primary">Go To Blog</Button>
         </Jumbotron>
     )
 }
